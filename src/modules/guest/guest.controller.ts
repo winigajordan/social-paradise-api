7426a3045ff328501a02ec4c7775b7330e7fa299
Stephane AKOLATSE <you@example.com>
2025-12-03 11:03:33 +0000

    Guest validation by slug

--- src/modules/guest/guest.controller.ts
+++ src/modules/guest/guest.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { GuestService } from './guest.service';
 
 @Controller('guest')
 export class GuestController {
-  constructor() {}
+  constructor(private readonly guestService: GuestService) {}
 
-
+  @Post(':slug')
+  async validateGuestBySlug(@Param('slug') slug: string) {
+    // console.log(slug);
+    return this.guestService.validateGuestBySlug(slug);
+  }
 }
--- src/modules/guest/guest.module.ts
+++ src/modules/guest/guest.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GuestService } from './guest.service';
 import { GuestController } from './guest.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Demand } from '../demand/entities/demand.entity';
+import { Guest } from './entities/guest.entity';
 
 @Module({
   controllers: [GuestController],
   providers: [GuestService],
+  imports: [
+    TypeOrmModule.forFeature([Demand, Guest])
+  ]
 })
 export class GuestModule {}
